0b52af10ec8a2ec9fb024ed2acb45b42da496a0a
Alozi <alozimozi@gmail.com>
2025-09-04 15:31:38 +0300

    Create first checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Checkbox from "@mui/material/Checkbox";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box sx={{ width: "100%", maxWidth: 800 }}>
+      <Typography variant="h3" component="h1">
+        Junior React Developer Roadmap
+      </Typography>
+      <FormGroup>
+        <FormControlLabel
+          control={<Checkbox defaultChecked />}
+          label="Learn HTML and CSS"
+        />
+        <FormControlLabel
+          control={<Checkbox defaultChecked />}
+          label="Learn JavaScript"
+        />
+        <FormControlLabel control={<Checkbox />} label="Learn TypeScript" />
+      </FormGroup>
+    </Box>
+  );
 }
 
-export default App
+export default App;
