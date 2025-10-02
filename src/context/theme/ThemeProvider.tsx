d786c3a106de989aa6718c126a20d8d71d34a606
Alozi <alozimozi@gmail.com>
2025-10-02 19:17:08 +0300

    Add MUI themes

--- src/context/theme/ThemeProvider.tsx
+++ src/context/theme/ThemeProvider.tsx
@@ -1,4 +1,8 @@
-import { useEffect, useState, type ReactNode } from "react";
+import { useEffect, useMemo, useState, type ReactNode } from "react";
+import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { darkTheme, lightTheme } from "./themes";
+
 import type { Theme } from "../../types/theme";
 import { ThemeContext } from "./ThemeContext";
 
@@ -15,9 +19,17 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
 
+  const mode = useMemo(
+    () => (theme === "light" ? lightTheme : darkTheme),
+    [theme]
+  );
+
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
+      <MuiThemeProvider theme={mode}>
+        <CssBaseline />
+        {children}
+      </MuiThemeProvider>
     </ThemeContext.Provider>
   );
 }
